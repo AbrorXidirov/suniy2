b08994ac2b7e3e92554cb63a113a2860f8edab08
AbrorXidirov <164007911+AbrorXidirov@users.noreply.github.com>
2024-12-07 11:00:48 +0500

    Update suniyweb.py

--- suniyweb.py
+++ suniyweb.py
@@ -11,14 +11,14 @@ with open('final_scaler.pkl', 'rb') as file:
     final_scaler = pickle.load(file)
 
 # Kiritish maydonlari
-st.title('Car Price Prediction')
+st.title('Avtomabil narxini bashorat qilish')
 
 # Foydalanuvchi uchun inputlar
-hp_kW = st.number_input('Horsepower (kW)', min_value=0, max_value=1000, value=66)
-age = st.number_input('Age (years)', min_value=0, max_value=50, value=2)
-km = st.number_input('Kilometers driven', min_value=0, max_value=500000, value=17000)
-make_model = st.selectbox('Make and Model', ['Audi A3', 'BMW X5', 'Mercedes Benz A-Class', 'Toyota Corolla'])
-gearing_type = st.selectbox('Gearing Type', ['Automatic', 'Manual'])
+hp_kW = st.number_input('Ot kuchi (kW)', min_value=0, max_value=1000, value=66)
+age = st.number_input('Chiqarilganiga qancha vaqt bo'lgani (years)', min_value=0, max_value=50, value=2)
+km = st.number_input('Yurgan masofasi', min_value=0, max_value=500000, value=17000)
+make_model = st.selectbox('Avtomabil modeli', ['Audi A3', 'BMW X5', 'Mercedes Benz A-Class', 'Toyota Corolla'])
+gearing_type = st.selectbox('Uzatish turi', ['Automatic', 'Manual'])
 
 # Model yordamida narxni bashorat qilish
 if st.button('Predict Price'):
