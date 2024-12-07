aa42ab9e350891f225349f220ed4566a6460d41c
AbrorXidirov <164007911+AbrorXidirov@users.noreply.github.com>
2024-12-07 10:56:02 +0500

    Update suniyweb.py

--- suniyweb.py
+++ suniyweb.py
@@ -11,17 +11,17 @@ with open('final_scaler.pkl', 'rb') as file:
     final_scaler = pickle.load(file)
 
 # Kiritish maydonlari
-st.title('Avtomobil Narxini Bashorat Qilish')
+st.title('Car Price Prediction')
 
 # Foydalanuvchi uchun inputlar
 hp_kW = st.number_input('Ot kuchi (kW)', min_value=0, max_value=1000, value=66)
-age = st.number_input('Ishlab chiqarilganiga qancha bo'lgan (yil)', min_value=0, max_value=50, value=2)
-km = st.number_input('Yurilgan masofa (km)', min_value=0, max_value=500000, value=17000)
-make_model = st.selectbox('Marka va Model', ['Audi A3', 'BMW X5', 'Mercedes Benz A-Class', 'Toyota Corolla'])
-gearing_type = st.selectbox('Uzatish turi', ['Avtomatik', 'Qo'lda'])
+age = st.number_input('Chiqarilganiga qancha bo'lgan (years)', min_value=0, max_value=50, value=2)
+km = st.number_input('Yurilgan masofa', min_value=0, max_value=500000, value=17000)
+make_model = st.selectbox('Modeli', ['Audi A3', 'BMW X5', 'Mercedes Benz A-Class', 'Toyota Corolla'])
+gearing_type = st.selectbox('Uzatish turi', ['Automatic', 'Manual'])
 
 # Model yordamida narxni bashorat qilish
-if st.button('Narxni bashorat qilish'):
+if st.button('Predict Price'):
     # Kirish qiymatlari
     my_dict = {
         "hp_kW": hp_kW,
@@ -38,6 +38,8 @@ if st.button('Narxni bashorat qilish'):
     my_dict = pd.get_dummies(my_dict)
 
     # X.columns bilan moslashtirish
+    # X.columns - bu treningda ishlatilgan ustunlar
+    # Bu qadamni faqatgina agar sizda treningda ishlatilgan ustunlar bo'lsa qilish kerak
     my_dict = my_dict.reindex(columns=['hp_kW', 'km', 'age', 'make_model_Audi A1', 'make_model_Audi A3',
                                        'make_model_Opel Astra', 'make_model_Opel Corsa', 'make_model_Opel Insignia',
                                        'make_model_Renault Clio', 'make_model_Renault Duster', 'make_model_Renault Espace',
@@ -47,7 +49,7 @@ if st.button('Narxni bashorat qilish'):
     my_dict_scaled = final_scaler.transform(my_dict)
 
     # Model yordamida narxni bashorat qilish
-    predicted_price = final_model.predict(my_dict_scaled)
+    predicted_price = 'lasso_model.predict(my_dict_scaled)
 
     # Natijani chiqarish
-    st.write(f'Avtomobilning bashorat qilingan narxi: ${predicted_price[0]:,.2f}')
+    st.write(f'The predicted price for the car is: ${predicted_price[0]:,.2f}')
